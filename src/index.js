34a561ef6bc8e169abf9042ce760d331d4423dd7
Sokol V <1470715@gmail.com>
2018-09-17 17:57:31 +0300

    fing triangle function

--- src/index.js
+++ src/index.js
@@ -4,4 +4,24 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+
+  var counter = 0;
+
+  for(let i = 0; i < preferences.length; i++){
+    
+    var firstNumber = preferences[i] - 1;
+    var secondNumber = preferences[firstNumber] - 1;
+    var thirdNumber = preferences[secondNumber] - 1;
+
+    if(firstNumber == i ||
+       firstNumber == secondNumber ||
+       secondNumber == thirdNumber) continue;
+
+    if(thirdNumber == i){
+      counter++;
+    }
+
+  }
+
+  return counter/3;
 };
